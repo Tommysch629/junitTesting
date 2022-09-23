59bda54c2ba444d9aee9fb2e14a66caee603305b
Tommy <Tommy@LAPTOP-61RIQ9U7>
2022-09-23 08:23:35 +0800

    test2

--- PokerProject/src/poker/Poker.java
+++ PokerProject/src/poker/Poker.java
@@ -69,9 +69,7 @@ public class Poker {
 	 *
 	 * @param args the arguments
 	 */
-	public static void main(String args[]) {
-		System.out.println(new Poker().isFullHouse(new String[] {"C2", "D2", "H2", "S3", "S4"}, 5));
-	}
+
 
 }
 // end of Poker.java
